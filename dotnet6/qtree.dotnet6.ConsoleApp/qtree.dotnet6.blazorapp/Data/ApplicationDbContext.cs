ec5a16241920624fff292276c9046b4114ac0c69
Edwin Lucke <edwin.lucke@movares.nl>
2021-12-13 10:47:43 +0100

    added default blazor webapp with signal R

---
+++ dotnet6/qtree.dotnet6.ConsoleApp/qtree.dotnet6.blazorapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace qtree.dotnet6.blazorapp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
